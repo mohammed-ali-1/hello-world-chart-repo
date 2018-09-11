56245316b7610b6f1238650ef61c988bd9e170c9
mohammed-ali-1 <mohammad10596@gmail.com>
2018-09-11 15:11:38 +0200

    Build and test2

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 responseString=`curl -X GET $HELMQA_URL'/livecheck?repo='$REPO_URL`
-statusCode=$responseString | jq.code
+statusCode=`$responseString | jq .code`
 
 if [ $statusCode -eq 404 ];
 then echo "ERROR! $responseString"
