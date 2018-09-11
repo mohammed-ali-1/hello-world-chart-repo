5d626d68b54eb5429314081f585e01d5cd5e807a
mohammed-ali-1 <mohammad10596@gmail.com>
2018-09-11 15:31:39 +0200

    Build and test3

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
+export HELMQA_URL=https://helmqa-helmqa.a3c1.starter-us-west-1.openshiftapps.com
+export REPO_URL=https://github.com/mohammed-ali-1/hello-world-chart-repo.git
 responseString=`curl -X GET $HELMQA_URL'/livecheck?repo='$REPO_URL`
-statusCode=`$responseString | jq .code`
-
-if [ $statusCode -eq 404 ];
-then echo "ERROR! $responseString"
+statusCode=`curl -X GET $HELMQA_URL'/livecheck?repo='$REPO_URL | jq .code`
+if [ "$statusCode" == "404" ]
+then echo "ERROR!\n $responseString"
 fi
