af96e2b2c8464f0d418a317903e978c895c350e6
mohammed-ali-1 <mohammad10596@gmail.com>
2018-10-09 17:19:30 +0200

    removed files

--- test.sh
+++
@@ -1,7 +0,0 @@
-export HELMQA_URL=https://helmqa-helmqa.a3c1.starter-us-west-1.openshiftapps.com
-export REPO_URL=https://github.com/mohammed-ali-1/hello-world-chart-repo.git
-responseString=`curl -X GET $HELMQA_URL'/livecheck?repo='$REPO_URL`
-statusCode=`curl -X GET $HELMQA_URL'/livecheck?repo='$REPO_URL | jq .code`
-if [ "$statusCode" == "404" ]
-then echo "ERROR!\n $responseString"
-fi
